1da3818b5f8f56cf00307bc97a201a71a1d8f53c
PrashastVats1 <prashastvats@gmail.com>
2023-09-25 00:03:18 +0530

    Using NUnit

---
+++ NUnitTestProj/NUnitTestProj/StudentTest.cs
@@ -0,0 +1,41 @@
+﻿using RainbowSchoolLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestProj
+{
+    [TestFixture]
+    public class StudentTests
+    {
+        [Test]
+        public void TestStudentName()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Name = "John Doe";
+
+            // Act
+            string studentName = student.Name;
+
+            // Assert
+            Assert.AreEqual("John Doe", studentName);
+        }
+
+        [Test]
+        public void TestStudentAddress()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Address = "123 Main St";
+
+            // Act
+            string studentAddress = student.Address;
+
+            // Assert
+            Assert.AreEqual("123 Main St", studentAddress);
+        }
+    }
+}
---
+++ NUnitTestProj/NUnitTestProj/TeacherTest.cs
@@ -0,0 +1,41 @@
+﻿using RainbowSchoolLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestProj
+{
+    [TestFixture]
+    public class TeacherTest
+    {
+        [Test]
+        public void TestTeacherName()
+        {
+            // Arrange
+            Teacher myTeacher = new Teacher();
+            myTeacher.Name = "Rajiv Kumar";
+
+            // Act
+            string teacherName = myTeacher.Name;
+
+            // Assert
+            Assert.AreEqual("Rajiv Kumar", teacherName);
+        }
+
+        [Test]
+        public void TestClassId()
+        {
+            // Arrange
+            Teacher myTeacher = new Teacher();
+            myTeacher.Id = 1;
+
+            // Act
+            int tecaherId = myTeacher.Id;
+
+            // Assert
+            Assert.AreEqual(1, tecaherId);
+        }
+    }
+}
